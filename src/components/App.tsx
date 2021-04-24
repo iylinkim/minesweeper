7ad8e1279fd68387c355cec6182db70075133d1d
iylinkim <kiylin94@naver.com>
2021-04-24 20:51:04 +0900

    Finished styles body and cells

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "scss/app.scss";
+import { generateCells } from "utils";
+import Button from "./Button";
 import NumberDisplay from "./NumberDisplay";
 
 const App: React.FC = () => {
-  return <div className="app">
+  const [cells, setCells] = useState(generateCells());
+
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => <Button key={`${rowIndex} - ${colIndex}`}/>)
+    );
+  };
+
+  return (
+    <div className="app">
       <div className="header">
-          <NumberDisplay value={0}/>
-          <div className="face"><span>😀</span></div>
-          <NumberDisplay value={23}/>
+        <NumberDisplay value={0} />
+        <div className="face">
+          <span>😀</span>
+        </div>
+        <NumberDisplay value={23} />
       </div>
-      <div className="body">Body</div>
-  </div>;
+      <div className="body">{renderCells()}</div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "scss/button.scss";
+
+const Button: React.FC = () => {
+  return <div className="button"></div>;
+};
+
+export default Button;
